dd5c5a5823d5303a213f1268eefd08ade26a6229
megazear7 <alexlockhart7@gmail.com>
2014-04-23 20:11:59 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Battleleague::Application.config.secret_key_base = '958725c10feb1828b704bdf54eed3fb2102d184f66c59378f1b7741cedab7b53f0ef5a69117cbf78c33602598ddafc7a2051cfaa3bb4e19a2b5f14d5512a9d24'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Battleleague::Application.config.session_store :cookie_store, key: '_battleleague_session'
