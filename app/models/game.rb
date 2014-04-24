5f00824d39fd59f89f408c1f954a0fdb65c843ee
megazear7 <alexlockhart7@gmail.com>
2014-04-24 12:48:15 -0400

    ready to start testing out the db model

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,7 +2,7 @@ class Game < ActiveRecord::Base
   belongs_to :map
   has_and_belongs_to_many :users
   has_many :armies
-  belongs_to :game_master, through: :game_masters, source: :user # TODO this line might not work, it might be expecting a has_many relationship
+  has_one :game_master, through: :game_masters, source: :user
 
   def players
     self.users
