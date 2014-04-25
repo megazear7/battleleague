3f2c1ea342d48cef20dbb9cda9e19286b5436df6
megazear7 <alexlockhart7@gmail.com>
2014-04-24 23:39:36 -0400

    figured out how to do it

--- app/models/map.rb
+++ app/models/map.rb
@@ -2,6 +2,14 @@ class Map < ActiveRecord::Base
   has_one :game
   has_many :spaces
 
+  def self.for_selection
+    maps = []
+    Map.all.each do |map|
+      maps << map if not map.game
+    end
+    maps
+  end
+
   def width
     self.spaces.maximum("x_cord")
   end
