9fa7180d68f84728557fdc78c9ed493e55d28314
megazear7 <alexlockhart7@gmail.com>
2014-04-25 11:45:26 -0400

    almost there, spaces are off by one right now though

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,10 +21,18 @@ module ApplicationHelper
     map.height.times do |y|
       map.width.times do |x|
         space = map.spaces.where(x_cord: x+1, y_cord: y+1).first
-        space = Space.new(x_cord: x+1, y_cord: y+1, terrain: "Terrain Unknown") if not space
+        space = Space.new(x_cord: x+1, y_cord: y+1, terrain: "Terrain Unknown", map: map) if not space
         yield space
       end
     end
+    html.html_safe
+  end
+
+  def space_display space
+    html = ""
+    html += "<span class='space' id='#{space.x_cord+1}-#{space.y_cord+1}' style='width: #{500/space.map.width}px; height: #{500/space.map.height}px; position: absolute; bottom: #{(510/space.map.height) * space.y_cord+1}px; left: #{(510/space.map.width) * space.x_cord+1}px;'>"
+    html += space.terrain
+    html += "</span>"
     html.html_safe
   end
 
