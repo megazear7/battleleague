acb09dd59244de1d3a6d20599dbe88321cbabe77
megazear7 <alexlockhart7@gmail.com>
2014-04-25 09:37:07 -0400

    added a helper for making the map. it needs some work...

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def map map
+    html = ""
+    map.height.times do |y|
+      html += "<div class='space-row'>"
+      map.width.times do |x|
+        space = map.spaces.where(x_cord: x+1, y_cord: y+1).first
+        space = Space.new(x_cord: x+1, y_cord: y+1, terrain: "Terrain Unknown") if not space
+        html += "<span class='space' id='#{x+1}-#{y+1}' style='width: #{500/map.width}px; height: #{500/map.height}px'>"
+        html += space.terrain
+        html += "</span>"
+      end
+      html += "</div>"
+    end
+    html.html_safe
+  end
+
 end
