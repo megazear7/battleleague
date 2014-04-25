2a52584381921293a7460f008af4ffb58c6d4937
megazear7 <alexlockhart7@gmail.com>
2014-04-25 15:51:37 -0400

    displaying armies now

--- app/models/army.rb
+++ app/models/army.rb
@@ -1,4 +1,14 @@
 class Army < ActiveRecord::Base
   belongs_to :user
   belongs_to :game
+
+  def close_to user
+    user.armies.where(game: self.game).each do |army|
+      if army.x_cord - self.x_cord < 2 or army.y_cord - self.y_cord < 2
+        return true
+      end
+    end
+    return false
+  end
+
 end
