a879c0f67c0c295c1414278ca6d052f2758d8997
megazear7 <alexlockhart7@gmail.com>
2014-04-25 15:28:28 -0400

    made helper methods for the map, I don't really like how it turned out though

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,23 +1,16 @@
 module ApplicationHelper
 
-  def map map
-    html = ""
-    map.height.times do |y|
-      html += "<div class='space-row'>"
-      map.width.times do |x|
-        space = map.spaces.where(x_cord: x+1, y_cord: y+1).first
-        space = Space.new(x_cord: x+1, y_cord: y+1, terrain: "Terrain Unknown") if not space
-        html += "<span class='space' id='#{x+1}-#{y+1}' style='width: #{500/map.width}px; height: #{500/map.height}px'>"
-        html += space.terrain
-        html += "</span>"
-      end
-      html += "</div>"
+  def display item, &block
+    if item.class == Map
+      return map item, &block
+    elsif item.class == Space
+      return space item, &block
     end
-    html.html_safe
   end
 
-  def test map
+  def map map
     html = ""
+    html = "<div class='map-container'>"
     map.height.times do |y|
       map.width.times do |x|
         space = map.spaces.where(x_cord: x+1, y_cord: y+1).first
@@ -25,12 +18,13 @@ module ApplicationHelper
         yield space
       end
     end
+    html += "</div>"
     html.html_safe
   end
 
-  def space_display space
+  def space space
     html = ""
-    html += "<span class='space' id='#{space.x_cord+1}-#{space.y_cord+1}' style='width: #{500/space.map.width}px; height: #{500/space.map.height}px; position: absolute; bottom: #{(510/space.map.height) * space.y_cord+1}px; left: #{(510/space.map.width) * space.x_cord+1}px;'>"
+    html += "<span class='space' id='#{space.x_cord}-#{space.y_cord}' style='width: #{500/space.map.width}px; height: #{500/space.map.height}px; position: absolute; bottom: #{(510/space.map.height) * (space.y_cord-1)}px; left: #{(510/space.map.width) * (space.x_cord-1)}px;'>"
     html += space.terrain
     html += "</span>"
     html.html_safe
