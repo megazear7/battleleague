c3a02d535c90e879548d5226b66fc4375a054c94
megazear7 <alexlockhart7@gmail.com>
2014-04-30 21:07:19 -0400

    fixed display issue with taking spaces when not a part of an alliance

--- app/models/space.rb
+++ app/models/space.rb
@@ -3,7 +3,7 @@ class Space < ActiveRecord::Base
   belongs_to :army
 
   def color current_user
-    if self.army and self.army.allied_to(current_user)
+    if self.army and ( self.army.allied_to(current_user) or current_user.armies.include?(self.army) )
       return "blue"
     elsif self.army
       return "red"
